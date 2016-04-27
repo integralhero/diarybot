557128dc330056f431257728551d273e8a885832
David Jiang <dnjiang@stanford.edu>
2016-04-27 15:47:01 -0700

    database test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -63,4 +64,16 @@ app.post('/webhook/', function (req, res) {
 })
 
 
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
 
