6f9f4cb415ba89e24aa36d38c5f877da372256ca
David Jiang <dnjiang@stanford.edu>
2016-05-11 15:26:33 -0700

    working wit

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.post('/webhook/', function (req, res) {
             //     }
                 
             // });
-            request.end();
+            // request.end();
             
         }
     }
