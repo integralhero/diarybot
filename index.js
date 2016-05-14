0cf28ba2182e90e47bdb3d1e3be58c671f9ead2f
David Jiang <dnjiang@stanford.edu>
2016-05-14 14:03:50 -0700

    fix for messaging event not defined

--- index.js
+++ index.js
@@ -97,8 +97,8 @@ app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
 app.post('/webhook/', function (req, res) {
-    messaging_events = req.body.entry[0].messaging
-
+    messaging_events = req.body.entry[0].messaging;
+    console.log(messaging_events);
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
         sender = event.sender.id
@@ -106,9 +106,7 @@ app.post('/webhook/', function (req, res) {
         if (event.message && event.message.text) {
             text = event.message.text
             const context = {};
-            singlesession = sender;
             console.log("hello received message");
-            const session = 'my-user-session-42';
             client.runActions(sessionId,text, sessions[sessionId].context, (error, context) => {
               if (error) {
                 console.log('Oops! Got an error: ' + error);
